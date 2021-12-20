c4d2a524d7f76e344f4b1c4083303616f5cd4b09
Reini Urban <rurban@cpan.org>
2021-12-20 20:01:14 +0100

    stabilize test-all-fast.sh
    disable test-texts with norm or profile defines

--- test-all-fast.sh
+++ test-all-fast.sh
@@ -8,9 +8,8 @@ make -f makefile.gnu clean
 for norm in NFKC NFC NFKD NFD FCC FCD; do
     for profile in 2 3 4 5 6; do
         echo "check -DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
-        make -f makefile.gnu check CFLAGS="-Wall -Wextra -O2 -DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
-        make -f makefile.gnu check-asan CFLAGS="-Wall -Wextra -O2 -DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
+        make -f makefile.gnu check DEFINES="-DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
+        make -f makefile.gnu check-asan DEFINES="-DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
         make -f makefile.gnu clean
     done
 done
-
