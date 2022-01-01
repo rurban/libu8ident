7d83e3a81525e827178bcd1ff14dbcab43e470a8
Reini Urban <rurban@cpan.org>
2022-01-01 13:21:18 +0100

    fix test-all-fast
    no DEFINES overrides, just add to it

--- test-all-fast.sh
+++ test-all-fast.sh
@@ -8,8 +8,8 @@ make -f makefile.gnu clean
 for norm in NFC NFD NFKC NFKD FCD FCC; do
     for profile in 2 3 4 5 6 C23_4 C11_6; do
         echo "check -DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
-        make -f makefile.gnu check DEFINES="-DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
-        make -f makefile.gnu check-asan DEFINES="-DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
+        make -f makefile.gnu check DEFS="-DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
+        make -f makefile.gnu check-asan DEFS="-DU8ID_NORM=$norm -DU8ID_PROFILE=$profile"
         make -f makefile.gnu clean
     done
 done
