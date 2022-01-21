31e75d53a84dc51cc0ba155aa475b23e4c8e089c
Reini Urban <rurban@cpan.org>
2022-01-21 17:48:57 +0100

    improve example
    better error message. fix 1st arg.
    use the outnorm API. (it's a bit awkward)

--- example.c
+++ example.c
@@ -25,7 +25,7 @@ static void usage(int exitcode) {
 
 int main(int argc, char **argv) {
   unsigned linenr = 0U;
-  enum u8id_options xid = U8ID_TR31_SAFEC23;
+  enum u8id_options xid = U8ID_TR31_XID;
   enum u8id_norm norm = U8ID_NFC;
   enum u8id_profile profile = U8ID_PROFILE_C23_4;
   unsigned u8idopts = (unsigned)xid;
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
 #endif
   }
   if (argc > 1) {
-    strncpy(path, argv[2], 255);
+    strncpy(path, argv[1], 255);
 #ifdef _WIN32
     path[255] = '\0'; // only windows does not terminate
 #endif
@@ -71,6 +71,10 @@ int main(int argc, char **argv) {
     // split on whitespace
     char *s = &line[0];
     while (*s) {
+      // we really should split on identifier boundaries, as in a real parser.
+      // but this is just a simple example. So we get lots of unneeded XID errors.
+      char word[256];
+      char *norm = NULL;
       char *p = strchr(s, ' ');
       ptrdiff_t len = p ? (ptrdiff_t)(p - s) : (ptrdiff_t)strlen(s);
       if (len && s[len - 1] == '\n')
@@ -78,8 +82,32 @@ int main(int argc, char **argv) {
       if (!len)
         break;
       int ret = u8ident_check_buf(s, len, NULL);
-      if (ret < 0) {
-        printf("\"%.*s\".%u: %d\n", (int)len, s, linenr, ret);
+      switch (ret) {
+      case U8ID_EOK_NORM:
+        if (len < 256) {
+          strncpy(word, s, len);
+          word[len] = '\0';
+          u8ident_check((uint8_t*)word, &norm);
+          printf("\"%s\".%u: NFC => \"%s\"\n", word, linenr, norm);
+          free(norm);
+        } else {
+          printf("\"%.*s\".%u: NFC ...\n", (int)len, s, linenr);
+        }
+        break;
+      case U8ID_ERR_XID:
+        printf("\"%.*s\".%u: Wrong XID U+%X\n", (int)len, s, linenr, u8ident_failed_char(0));
+        break;
+      case U8ID_ERR_SCRIPT:
+      case U8ID_ERR_SCRIPTS:
+        {
+          uint32_t cp = u8ident_failed_char(0);
+          const char* scr = u8ident_failed_script_name(0);
+          printf("\"%.*s\".%u: Wrong SCRIPT %s for U+%X, have %s\n", (int)len, s,
+                 linenr, scr, cp, u8ident_script_name(u8ident_get_script(cp)));
+          free((char*)scr);
+          break;
+        }
+      default: break;
       }
       if (p)
         s = p + 1;
